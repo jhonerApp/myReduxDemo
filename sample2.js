fd46dd3461f331b67e3200c527c246e0d07f3973
jhonerApp <jhonerApp@gmail.com>
2020-12-06 06:50:17 +0800

    Commit

--- sample2.js
+++ sample2.js
@@ -5,7 +5,6 @@ export default class sample2 extends Component {
         return (
             <div>
                 <h1>Sample</h1>
-                <h2>Sample</h2>
             </div>
         )
     }
