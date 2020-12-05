0bb61206a8a0028fa3166abcb0379d326317000c
jhonerApp <jhonerApp@gmail.com>
2020-12-06 00:48:24 +0800

    Update all changes

--- sample.js
+++ sample.js
@@ -1,2 +1,21 @@
-import React  from "react"
-console.log('Testing');
+import React, { Component } from 'react'
+
+export default class sample extends Component {
+    constructor(){
+        super();
+        // eslint-disable-next-line no-labels
+        state:{
+            
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h1>
+                    Testing
+                </h1>
+            </div>
+        )
+    }
+}
+
