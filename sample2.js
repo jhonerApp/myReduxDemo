7cd00ff43305e4207b512fe63d3910852b01beca
jhonerApp <61251872+jhonerApp@users.noreply.github.com>
2020-12-06 06:49:48 +0800

    Update sample2.js

--- sample2.js
+++ sample2.js
@@ -4,8 +4,7 @@ export default class sample2 extends Component {
     render() {
         return (
             <div>
-                <h1>Sample</h1>
-                <h2>Sample</h2>
+            
             </div>
         )
     }
